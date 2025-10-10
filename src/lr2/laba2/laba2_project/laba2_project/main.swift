fddffa3612de86a556368a8f2caa50a501c8412c
srtepa <stepankonon1@gmail.com>
2025-10-10 20:46:24 +0300

    task2 all

--- src/lr2/laba2/laba2_project/laba2_project/main.swift
+++ src/lr2/laba2/laba2_project/laba2_project/main.swift
@@ -5,3 +5,8 @@ print("Task 1")
 print(addFractions(1, 2, 1, 2))
 print(addFractions(1, 3, 1, 2))
 print(addFractions(1, 4, 1, 4))
+
+print("\nTask 2")
+numOfOdd([1, 2, 3, 4, 5, 6, 7])
+numOfWords("Привет мир как дела")
+numOfWords("  Много   пробелов  ")
--- src/lr2/laba2/laba2_project/laba2_project/task2.swift
+++ src/lr2/laba2/laba2_project/laba2_project/task2.swift
@@ -1,8 +1,19 @@
-//
-//  task2.swift
-//  laba2_project
-//
-//  Created by Степан Конон on 10.10.25.
-//
-
 import Foundation
+
+func numOfOdd(_ array: [Int]) {
+    var count = 0
+    
+    for number in array {
+        if number % 2 != 0 {
+            count += 1
+        }
+    }
+    
+    print("Число нечетных чисел в массиве: \(count)")
+}
+
+func numOfWords(_ str: String) {
+    let words = str.split(separator: " ").filter { !$0.isEmpty }
+    let count = words.count
+    print("Число слов в строке: \(count)")
+}
