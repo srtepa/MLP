cecefe178a9e62d17dfb380c6a8c4f22971dde71
srtepa <stepankonon1@gmail.com>
2025-10-30 22:26:01 +0300

    ctreate function method

--- src/lr2/laba2/laba2_project/laba2_project/main.swift
+++ src/lr2/laba2/laba2_project/laba2_project/main.swift
@@ -7,9 +7,13 @@ print(addFractions(1, 3, 1, 2))
 print(addFractions(1, 4, 1, 4))
 
 print("\nTask 2")
-print("Число нечетных чисел в массиве: \(numOfOdd([1, 2, 3, 4, 5, 6, 7]))")
-print("Число слов в строке: \(numOfWords("Привет мир как дела"))")
-print("Число слов в строке: \(numOfWords("     Много     Пробелов   "))")
+
+let funOdd: ([Int]) -> Int = numOfOdd(_:)
+print("Число нечетных чисел в массиве: \(funOdd([1, 2, 3, 4, 5, 6, 7]))")
+
+let funStr: (String) -> Int = numOfWords(_:)
+print("Число слов в строке: \(funStr("Привет мир как дела"))")
+print("Число слов в строке: \(funStr("     Много     Пробелов   "))")
 
 print("\nПример использования перечисления")
 print(operateFractions(1, 2, 1, 3, op: .addition))
--- src/lr2/laba2/laba2_project/laba2_project/task2.swift
+++ src/lr2/laba2/laba2_project/laba2_project/task2.swift
@@ -2,9 +2,9 @@ import Foundation
 
 
 func numOfOdd(_ array: [Int]) -> Int {
-    array.filter { $0 % 2 != 0 }.count
+    return array.filter { $0 % 2 != 0 }.count
 }
 
 func numOfWords(_ str: String) -> Int {
-    str.split(separator: " ").filter { !$0.isEmpty }.count
+    return str.split(separator: " ").filter { !$0.isEmpty }.count
 }
