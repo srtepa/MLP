596987f05932c0d40342184b858514c2a2e8c10f
srtepa <stepankonon1@gmail.com>
2025-11-11 17:38:55 +0300

    solve lr3

---
+++ src/lr3/laba3/laba3_project/laba3_project/MagicSquare.swift
@@ -0,0 +1,13 @@
+public class MagicSquare{
+    private let matrix: [[Int]]//приватные поля класса
+    private let size: Int
+    
+    init(matrix: [[Int]]){//конструктор класса
+        self.matrix = matrix
+        self.size = matrix.count
+    }
+    
+    func isSquare() -> Bool{
+        
+    }
+}
